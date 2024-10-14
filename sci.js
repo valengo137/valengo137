79799807bb14801cb5c078508efb337b3fdb0416
Giang Ngo <valegs137@gmail.com>
2024-10-15 00:02:09 +0700

    alot.
    update homepage, update the intro (finish, plus divider), update header, add new font, start trajectory for tomorrow.

--- sci.js
+++ sci.js
@@ -2,11 +2,13 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "2rem";
-    document.getElementById("header").style.height = "10vh";
+    document.getElementById("header").style.fontSize = "3rem";
+    document.getElementById("header").style.height = "15vh";
+    document.getElementById("header").style.zIndex = "2";
+
   } else {
-    document.getElementById("header").style.fontSize = "1rem";
     document.getElementById("header").style.height = "13vh";
+    document.getElementById("header").style.fontSize = "1.54rem";
   }
 }
 // Code taken from https://www.w3schools.com/howto/howto_js_shrink_header_scroll.asp - tutorial
